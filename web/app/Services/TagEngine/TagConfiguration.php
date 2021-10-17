35ddaf4a38f685136414652d36cf22d49cac7407
Ammon Smith <ammon.i.smith@gmail.com>
2021-10-17 05:19:47 -0400

    Convert lists to sets in constructor.

--- web/app/Services/TagEngine/TagConfiguration.php
+++ web/app/Services/TagEngine/TagConfiguration.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Wikijump\Services\TagEngine;
 
+use Ds\Set;
 
 class TagConfiguration
 {
@@ -43,6 +44,16 @@ class TagConfiguration
     {
         $this->tags = $data->tags ?? [];
         $this->tag_groups = $data->tag_groups ?? [];
+
+        // Convert $tags
+        foreach ($this->tags as &$tag) {
+            $tag['properties']['role_ids'] = new Set($tag['properties']['role_ids']);
+        }
+
+        // Convert $tag_groups
+        foreach ($this->tag_groups as &$tag_group) {
+            $tag_group['members'] = new Set($tag_group['members']);
+        }
     }
 
     public function toJson(): array
